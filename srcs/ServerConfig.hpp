1b223389d3c0f7226f87574e52e9c37418083ad4
Md Ohiduzzaman Sumon <sumon@Mds-MacBook-Air.fritz.box>
2024-10-01 19:48:54 +0200

    config parsing-> made files for each class

---
+++ srcs/ServerConfig.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include "LocationConfig.hpp"
+
+// --> If you want to access the config values, initialize the ServerConfig class
+// --> The ServerConfig class will have the server directives
+// --> use the get functions to access the values
+
+class ServerConfig : public LocationConfig
+{
+    private:
+        std::string listenPort;
+        std::string serverName;
+        std::string errorPage;
+        std::vector<LocationConfig> locations;
+        std::vector<ServerConfig> servers;
+
+    public:
+        ServerConfig();
+        ServerConfig(std::string configFile);
+        ~ServerConfig();
+
+        void displayConfig();
+        std::string getListenPort();
+        std::string getServerName();
+        std::string getErrorPage();
+        std::vector<LocationConfig> getLocations();
+        std::vector<ServerConfig> getServers();
+
+        void serverBlock(std::string line, size_t &i, std::vector<std::string> configVector, ServerConfig &server, std::string configFile);
+        void locationBlock(std::string line, size_t &i, std::vector<std::string> configVector, ServerConfig &server, std::string configFile);
+};
