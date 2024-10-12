d63803cdcd6001e980997ebc03ea7916148e7b63
BenzThor <benzthorben@gmail.com>
2024-10-12 11:17:45 +0200

    adapted the structure of Epoll to be more concise, readable and handle events better

--- srcs/Epoll.hpp
+++ srcs/Epoll.hpp
@@ -54,6 +54,7 @@ private:
  	* and attempts to accept a new client connection if so.
  	* Returns true if a new client connection was accepted, otherwise false.
  	*/
+	int		checkEpollWait(int);
 	// ------------- Client Handling -------------
 	bool	NewClient(vSrv&, int); // DISCUSS: possible change: implement a flag that server does not accept new connections anymore to be able to shut it down
 	/**
@@ -61,28 +62,32 @@ private:
 	 * Returns true if the new client was successfully accepted, otherwise false.
 	 */
 	bool	AcceptNewClient(Server&, lstSocs::iterator&);
-
+	// check how the event should be handled for an existing client
+	void	existingClient(vSrv&, uint32_t, int);
 	// returns the client corresponding to the fd passed
 	Client*	retrieveClient(vSrv&, int);
+	// outputs the corresponding error message and removes the client from epoll
+	void	clientRetrievalError(int);
+	// outputs the corresponding error message and removes the client from epoll and the corresponding server
+	void	clientErrorOrHungUp(Client*);
 	/**
 	 * Handles communication with an existing client by reading data from the
 	 * specified file descriptor. Responds to the client with a simple message
 	 * and manages socket state on errors or disconnections.
 	 * Returns 0 on success, -1 on errors or client disconnection.
 	 */
-	int		ExistingClient(Client*);
-
+	int		clientRequest(Client*);
 	// DISCUSS: should be handled in Request
 	void	validRequest(Server* serv, std::vector<char> buffer, ssize_t count, Request& request);
 	void	validRequest(std::vector<char>, ssize_t, Request&);
 	int		invalidRequest(Client*);
 	int		emptyRequest(Client*);
 
+	void	clientResponse(Client*);
+
 	// ------------- Cleanup -------------
 	// removes the fd from the clients
 	void	removeClientFromServer(Server*, int);
-	// removes fd from clients and from epoll and closes it
-	void	removeClient(Server*, int);
 public:
 	Epoll();
 	~Epoll();
@@ -99,6 +104,8 @@ public:
 	// ------------- Cleanup -------------
 	// removes the fd from epoll and closes the fd
 	void	removeClientEpoll(int);
+	// removes fd from clients and from epoll and closes it
+	void	removeClient(Client*);
 
 	// ------------- Getters -------------
 	// returns the epoll fd
