719ca8d362729de738e214184b1db1a0ce25c2d2
Md Ohiduzzaman Sumon <msumon@c4r7p6.42vienna.com>
2024-10-12 16:59:28 +0200

    errors now work with config error page, if no error page is config it will take a default error page

--- srcs/ErrorHandle.hpp
+++ srcs/ErrorHandle.hpp
@@ -3,38 +3,49 @@
 #pragma once
 
 #include "ServerConfig.hpp"
+#include "Client.hpp"
+#include "Response.hpp"
+#include "Server.hpp"
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <sstream>
 
+class Client;
+class ServerConfig;
+class Server;
 
-class ErrorHandle : public ServerConfig
+class ErrorHandle
 {
     private:
         std::string errorFile;
-        size_t errorStatusCode;
+        std::string errorStatusCode;
         std::string errorMessage;
+        std::string pageTitle;
+        std::string errorBody;
+        std::string newErrorFile;
 
         std::vector<ErrorHandle> errorVector;
 
     public:
         ErrorHandle();
-        ErrorHandle(std::string configFile);
         ~ErrorHandle();
 
         void setErrorFile(std::string errorFile);
-        void setErrorStatusCode(size_t errorStatusCode);
+        void setErrorStatusCode(std::string errorStatusCode);
         void setErrorMessage(std::string errorMessage);
 
         std::string getErrorFile();
-        size_t getErrorStatusCode();
+        std::string getErrorStatusCode();
         std::string getErrorMessage();
         std::vector<ErrorHandle> getErrorVector();
+        std::string getNewErrorFile();
         
         void displayError();
-        void modifyErrorPage();
+        void prepareErrorPage(Client *client, std::string statusCode);
+        std::string modifyErrorPage();
         
         template <typename T>
         std::string to_string(const T& value)
