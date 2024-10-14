5ff56403e35c10766f803c62c63fadfec49d2877
Benjamin Pochlauer <bpochlau@c4r7p10.42vienna.com>
2024-10-14 14:52:10 +0200

    prepared for chunkstring

--- srcs/Response.cpp
+++ srcs/Response.cpp
@@ -63,7 +63,7 @@ std::string Response::getActualTimeString() {
 	return ss.str();
 }
 
-static std::string createRequestString(Request& request, std::string& body, std::string statusCode) {
+static std::string createHeaderString(Request& request, std::string& body, std::string statusCode) {
 	std::stringstream ss;
 	std::string statusMessage = Response::statusCodes.find(statusCode)->second;
 
@@ -77,16 +77,16 @@ static std::string createRequestString(Request& request, std::string& body, std:
 	return ss.str();
 }
 
-static std::string createRequestAndBodyString(Request& request, std::string& body, std::string statusCode) {
+static std::string createHeaderAndBodyString(Request& request, std::string& body, std::string statusCode) {
 	std::stringstream ss;
-	ss << createRequestString(request, body, statusCode) << "\n";
+	ss << createHeaderString(request, body, statusCode) << "\n";
 	ss << body << "\n"; //BP: \n at the end - do we need this?
 
 	return ss.str();
 }
 
 void	Response::header(int socketFd, Request& request, std::string& body) {
-	std::string headString = createRequestString(request, body, "200");
+	std::string headString = createHeaderString(request, body, "200");
 	write(socketFd , headString.c_str(), headString.size());
 }
 
@@ -94,7 +94,7 @@ void	Response::headerAndBody(int socketFd, Request& request, std::string& body) 
 	if (body.size() > CHUNK_SIZE) {
 		sendWithChunkEncoding(socketFd, request, body);
 	} else {
-		std::string totalString = createRequestAndBodyString(request, body, "200");
+		std::string totalString = createHeaderAndBodyString(request, body, "200");
 		// std::cout << totalString << std::endl;
 		write(socketFd , totalString.c_str(), totalString.size());
 	}
@@ -110,7 +110,7 @@ void	Response::FallbackError(int socketFd, Request& request, std::string statusC
 	ss << "<body>\n<center><h1>" << statusCode << " " << statusMessage << "</h1></center>\n";
 	ss << "<hr><center>" << "WEBSERV OR SERVERNAME?" << "</center>\n</body>\n</html>\n";
 	std::string body = ss.str();
-	std::string totalString = createRequestAndBodyString(request, body, statusCode);
+	std::string totalString = createHeaderAndBodyString(request, body, statusCode);
 	write(socketFd, totalString.c_str(), totalString.size());
 }
 
@@ -137,7 +137,7 @@ void	Response::FallbackError(int socketFd, Request& request, std::string statusC
 		//-- this will modify the error page replacing the status code, message and page title
 		//-- this will return the modified error page as a string
 		std::string errorBody = errorHandle.modifyErrorPage();
-		std::string totalString = createRequestAndBodyString(request, errorBody, statusCode);
+		std::string totalString = createHeaderAndBodyString(request, errorBody, statusCode);
 		writeReturn = write(socketFd, totalString.c_str(), totalString.size());
 		if (writeReturn == -1)
 			throw std::runtime_error("Error writing to socket in Response::FallbackError!!");
@@ -154,7 +154,7 @@ void	Response::FallbackError(int socketFd, Request& request, std::string statusC
 		ss << "<body>\n<center><h1>" << statusCode << " " << statusMessage << "</h1></center>\n";
 		ss << "<hr><center>" << "WEBSERV OR SERVERNAME?" << "</center>\n</body>\n</html>\n";
 		std::string body = ss.str();
-		std::string totalString = createRequestAndBodyString(request, body, statusCode);
+		std::string totalString = createHeaderAndBodyString(request, body, statusCode);
 		writeReturn = write(socketFd, totalString.c_str(), totalString.size());
 		if (writeReturn == -1)
 			throw std::runtime_error("Error writing to socket in Response::FallbackError!!");
@@ -166,9 +166,8 @@ void	Response::FallbackError(int socketFd, Request& request, std::string statusC
 void	Response::sendChunks(int socketFd, std::string chunkString) {
 	std::ostringstream ss;
 	ss << std::hex << chunkString.size() << "\r\n";
+	ss << chunkString << "\r\n";
 	send(socketFd, ss.str().c_str(), ss.str().size(), 0);
-	send(socketFd, chunkString.c_str(), chunkString.size(), 0);
-	send(socketFd, "\r\n", 2, 0);
 
 	usleep(100000); // change to
 
@@ -176,12 +175,14 @@ void	Response::sendChunks(int socketFd, std::string chunkString) {
 
 void	Response::sendWithChunkEncoding(int socketFd, Request& request, std::string& body) {
 	(void) request;
-	std::string headers = "HTTP/1.1 200 OK\r\n"
+	std::string ChunkStartHeader = "HTTP/1.1 200 OK\r\n"
                           "Content-Type: video/mp4\r\n"
                           "Transfer-Encoding: chunked\r\n"
                           "\r\n";
-	//HeaderSent false? then:
-	send(socketFd, headers.c_str(), headers.size(), 0);
+	//ChunkStartHeaderSent false? then:
+	send(socketFd, ChunkStartHeader.c_str(), ChunkStartHeader.size(), 0);
+
+	
 	for (unsigned long i = 0; i < body.size(); i += CHUNK_SIZE) { // add bytesSent
 		sendChunks(socketFd, body.substr(i, CHUNK_SIZE));
 	}
