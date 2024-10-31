ee020fad1441defc8a7d504ecf71b6aadf40b883
Sumon Md Ohiduzzaman <117649754+sumon-ohid@users.noreply.github.com>
2024-10-31 09:33:20 +0000

    cgi can handle bigger files types

--- www/html/cgi-bin/mime.sh
+++ www/html/cgi-bin/mime.sh
@@ -3,4 +3,4 @@
 echo -e 'HTTP/1.1 200 OK\n'
 echo -e 'Content-Type: image/png\r\n'
 echo -e '\r\n\r\n'
-cat /project/workspace/www/html/cgi-bin/mime.png
+cat /project/workspace/www/html/cgi-bin/mime2.png
